2a99e0251247180dfc082d3333f6193fdcc88568
yosef <jolarohe@gmail.com>
2025-01-09 02:44:13 +0300

    f

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+   <div>
+    <h1>Hello World</h1>
+    <Button variant={"destructive"}>Click me</Button>
+    
+   </div>
+  );
+}
