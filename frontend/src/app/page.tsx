c41c921e2a7b677bb2ec2fa3086d0d76a1961e94
yosef <jolarohe@gmail.com>
2025-01-09 03:00:25 +0300

    installing shadcn

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,22 @@
-import Image from "next/image";
+"use client"; 
+
 
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+
+
+
+
+
+
+
+
   return (
    <div>
     <h1>Hello World</h1>
     <Button variant={"destructive"}>Click me</Button>
+
     
    </div>
   );
