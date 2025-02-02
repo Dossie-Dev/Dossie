2bde46977b2166d39349f467289b8b9e8df37c56
Yohannes Mesganaw <yohannesmesg@gmail.com>
2025-02-02 09:45:26 +0300

    procted the routes for all the vauels

--- backend/routes/researchRoutes.js
+++ backend/routes/researchRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { getAllResearchPapers, getOneResearchPaper, updateOneResearchPaper, deleteOneResearchPaper } = require("../controller/researchController/researchController");
 const router = express.Router();
+const {getAllResearchPapers, getOneResearchPaper, updateOneResearchPaper, deleteOneResearchPaper} = require("../controller/researchController/researchController");
 
 
 router.route("/").get(getAllResearchPapers);
